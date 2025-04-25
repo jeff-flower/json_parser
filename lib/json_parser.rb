f514b1136bb982bbe065d8061c19a0f4fe69580e
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2025-04-25 12:14:14 -0600

    Remove over-optimization

--- lib/json_parser.rb
+++ lib/json_parser.rb
@@ -1,9 +1,7 @@
 # JsonParser module
 module JsonParser
   class JSONParser
-    def self.valid?(raw_string)
-      string = raw_string.chomp.strip
-
+    def self.valid?(string)
       string.start_with?('{') && string.end_with?('}')
     end
   end
