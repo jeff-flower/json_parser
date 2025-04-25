3b9774b9fcc0e88d7f15ef1e204004d4fdc47fe8
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2025-04-25 12:17:53 -0600

    Add next two test cases

--- spec/json_parser_spec.rb
+++ spec/json_parser_spec.rb
@@ -3,16 +3,40 @@ require './lib/json_parser'
 describe JsonParser do
   describe '#valid?' do
     # this needs to run from the command line, I'm thinking we can worry about that later
-    it 'recognizes {} as valid JSON' do
-      input = '{}'
+    context 'Step 1' do
+      it 'recognizes {} as valid JSON' do
+        input = '{}'
 
-      expect(JsonParser::JSONParser.valid?(input)).to be true
+        expect(JsonParser::JSONParser.valid?(input)).to be true
+      end
+
+      it 'recognizes {{ as invalid JSON' do
+        input = '{{'
+
+        expect(JsonParser::JSONParser.valid?(input)).to be false
+      end
+
+      it 'recognizes an empty string as invalid JSON' do
+        input = ''
+
+        expect(JsonParser::JSONParser.valid?(input)).to be false
+      end
     end
 
-    it 'recognizes {{ as invalid JSON' do
-      input = '{{'
+    context 'Step 2' do
+      it 'recognizes {"key": "value",} as invalid JSON' do
+        input = '{"key": "value",}'
 
-      expect(JsonParser::JSONParser.valid?(input)).to be false
+        expect(JsonParser::JSONParser.valid?(input)).to be false
+      end
+
+      it 'recognizes {"key": "value"} as valid JSON' do
+        input = '{"key": "value"}'
+
+        expect(JsonParser::JSONParser.valid?(input)).to be true
+      end
+
+      # NEXT: Test cases for spec/testCases/step2/invalid2.json and valid2.json
     end
 
     # NEXT: use tests from dropbox (see link in challenge)
