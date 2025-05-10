9b6f8a242eedee57babb76be52e315926463a3dd
Jeff Flower <jeff.flower14@gmail.com>
2025-05-09 23:44:04 -0600

    Curly braces pass with lexer (string fails)

--- lib/json_parser.rb
+++ lib/json_parser.rb
@@ -1,11 +1,29 @@
+require './lib/json_lexer'
+
 # JsonParser module
 module JsonParser
   class JSONParser
     def self.valid?(string)
+      stack = []
+      tokens = JsonLexer::JSONLexer.lex(string)
+      tokens.each do |token|
+        # ???
+        token_type = token[1]
+        case token_type
+        when 'LEFT_BRACE'
+          stack.push(token)
+        when 'RIGHT_BRACE'
+          stack.pop
+        end
+      end
+
+      stack.empty?
+
+      # OLD
       # string.start_with?('{') && string.end_with?('}')
       # this is a terrible idea
-      re = /^{\s*"?\w*"?\s*:?\s*"?\w*"?}/
-      re.match?(string)
+      # re = /^{\s*"?\w*"?\s*:?\s*"?\w*"?}/
+      # re.match?(string)
     end
   end
 end
